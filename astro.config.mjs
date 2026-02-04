fbc9aca623d4531bd4c289cdb3931b08e7806903
patukaelmago <patukaelmago@gmail.com>
2026-02-04 02:14:16 +0000

    fix: pages base

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import compress from 'astro-compress';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
 
-// https://astro.build/config
 export default defineConfig({
-  experimental: {
-    viewTransitions: true,
-  },
+  site: "https://patukaelmago.github.io",
+  base: "/estudioIrigoyen/",
+  experimental: { viewTransitions: true },
   integrations: [tailwind(), compress()],
 });
